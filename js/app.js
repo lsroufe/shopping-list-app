9b3b13ba532c148e0cef043f31172a627811dc83
Logan Sroufe <lsroufe@gmail.com>
2016-05-24 20:00:22 -0700

    first commit

---
+++ js/app.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+	$('form').on('submit', function(e) {
+		e.preventDefault();
+		console.log('form submitted');
+
+	});
+	// on form submit
+	//  get input text value
+	//  create new label element with checkbox, text and delete button
+	//  preappend or append the new label to the div
+});
